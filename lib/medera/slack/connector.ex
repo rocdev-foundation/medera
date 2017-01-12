3fc3969327b2b587fdf1e743a0afe59e487cd5c6
Dan Swain <dan.t.swain@gmail.com>
2017-01-11 20:52:42 -0500

    Resolve user and channel names

--- lib/medera/slack/connector.ex
+++ lib/medera/slack/connector.ex
@@ -8,6 +8,7 @@ defmodule Medera.Slack.Connector do
 
   alias Slack.Bot
   alias Slack.Sends
+  alias Slack.Lookups
 
   use Slack
 
@@ -22,8 +23,12 @@ defmodule Medera.Slack.Connector do
   end
 
   @doc false
-  def handle_event(event, _slack, state) do
-    :ok = MederaSlack.receive_event(event)
+  def handle_event(event, slack, state) do
+    :ok = event
+    |> resolve_user(slack)
+    |> resolve_channel(slack)
+    |> MederaSlack.receive_event
+
     {:ok, state}
   end
 
@@ -36,4 +41,32 @@ defmodule Medera.Slack.Connector do
     Logger.warn("#{__MODULE__} is ignoring unexpected message #{inspect msg}")
     {:ok, process_state}
   end
+
+  # `:user` is a slack id - this resolves that to a human-readable
+  #    user id if one is available
+  defp resolve_user(event = %{user: user}, slack) do
+    Map.put(event, :human_user, Lookups.lookup_user_name(user, slack))
+  end
+  defp resolve_user(event, _), do: event
+
+  # `:channel` is a slack id - this resolves that to a human-readable
+  #    channel name if one is available and marks the message as a DM if it is
+  defp resolve_channel(event = %{channel: channel}, slack) do
+    case channel do
+      # channel
+      "C" <> _ ->
+        Map.put(
+          event,
+          :human_channel,
+          Lookups.lookup_channel_name(channel, slack)
+        )
+      # direct message
+      "D" <> _ ->
+        event
+        |> Map.put(:human_channel, Lookups.lookup_user_name(channel, slack))
+        |> Map.put(:direct_message, true)
+    end
+  end
+  defp resolve_channel(event, _), do: event
+
 end
--- lib/medera/slack/event.ex
+++ lib/medera/slack/event.ex
@@ -10,7 +10,10 @@ defmodule Medera.Slack.Event do
     text: nil,
     ts: nil,
     user: nil,
-    channel: nil
+    human_user: nil,
+    channel: nil,
+    human_channel: nil,
+    direct_message: false
   )
   @type t :: %__MODULE__{}
 
@@ -23,12 +26,15 @@ defmodule Medera.Slack.Event do
   def from_slack(event) do
     %Event{
       type: event_type(event),
+      direct_message: Map.get(event, :direct_message, false),
       # these might not be set, so use Map.get which will safely set them to
       # nil if they are not present
       channel: Map.get(event, :channel),
+      human_channel: Map.get(event, :channel),
       text: Map.get(event, :text),
       ts: Map.get(event, :ts),
-      user: Map.get(event, :user)
+      user: Map.get(event, :user),
+      human_user: Map.get(event, :human_user)
     }
   end
 
@@ -38,6 +44,8 @@ defmodule Medera.Slack.Event do
   defp event_type(%{type: "hello"}),           do: :hello
   # when a user is setting/unsetting 'away'
   defp event_type(%{type: "presence_change"}), do: :presence_change
+  # user opens a direct message channel
+  defp event_type(%{type: "im_open"}),         do: :im_open
   # experimental: http://api.slack.com/events/reconnect_url
   defp event_type(%{type: "reconnect_url"}),   do: :reconnect_url
   defp event_type(event) do
